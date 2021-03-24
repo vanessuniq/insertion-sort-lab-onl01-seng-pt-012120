1ba89d71c4f1eaa5d177e990d953c80c1a5af54b
vanessuniq <vanessuniq@gmail.com>
2021-03-24 18:46:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-function findMinAndRemove(array){
+function findMinAndRemove(array) {
+    let min = array[0],
+        minIndex = 0;
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+            minIndex = i;
+        }
 
+    };
+    array.splice(minIndex, 1);
+    return min;
 }
 
-function selectionSort(array){
-
+function selectionSort(array) {
+    const sortedArray = [];
+    while (array.length !== 0) {
+        sortedArray.push(findMinAndRemove(array));
+    };
+    return sortedArray;
 }
